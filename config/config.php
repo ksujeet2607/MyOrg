5c1a0c2645b50a7a49b5003ab7a31ed6e4d22fd7
Sujeet Kumar Kahar <kahar26sujeet@gmail.com>
2020-08-06 01:19:34 +0530

    config ignore

--- config/config.php
+++ config/config.php
@@ -1,10 +1,10 @@
 <?php
 // database credentials
 
-define('DB_NAME', 'technetdb');
-define('DB_USER', 'technetteam');
-define('DB_PASSWORD', 'Technets@1231_#');
-define('DB_HOST', '202.66.175.41');
+define('DB_NAME', 'technetsdb');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
 
 error_reporting(0);
 ini_set("display_errors", "off");
@@ -13,15 +13,15 @@ ini_set("display_errors", "off");
 // ini_set("display_errors", "on");
 
 
-define('BASE_URL', 'http://test.technets.in/');
-define('PUBLIC_URL', 'http://test.technets.in/u/');
-define('ADMIN_URL', 'http://test.technets.in/admin/');
-define('ADMUR_URL', 'http://test.technets.in/adminuser/');
+define('BASE_URL', 'http://localhost/TechNets/');
+define('PUBLIC_URL', 'http://localhost/TechNets/u/');
+define('ADMIN_URL', 'http://localhost/TechNets/admin/');
+define('ADMUR_URL', 'http://localhost/TechNets/adminuser/');
 
-define('SRC_URL', 'http://test.technets.in/assets/'); // Load Js/Css
+define('SRC_URL', 'http://localhost/TechNets/assets/'); // Load Js/Css
 
-define('IMG_URL', 'http://test.technets.in/images/');// load Images
-define('GAL_URL', 'http://test.technets.in/gallery/'); // load Gallery images
+define('IMG_URL', 'http://localhost/TechNets/images/');// load Images
+define('GAL_URL', 'http://localhost/TechNets/gallery/'); // load Gallery images
 
 
 define("SCANFILE", "scan");
@@ -37,7 +37,7 @@ define('SMS_PASSWORD', "");
 
 define('CONTACT', "+91 93430 86888");
 define('CONTACT_NO', "9343086888");
-define('EMAIL_FROM', "info@technets.in");
+define('EMAIL_FROM', "jainCommunity@gmail.com");
 
 define('EMAIL_FROM1', "noreply.matrimony.com@gmail.com");
 define('EMAIL_PASS1', "Admin@123");
