1406a7a58576d54fc89a2371b8cb4345ed7488e3
gautam <gautamkumar880@gmail.com>
2020-08-23 15:47:09 +0530

    upload

--- view/user/index.php
+++ view/user/index.php
@@ -1,18 +1,33 @@
 
 
-            <section class="full-section features-section fancy-shadow">
-                <div class="container">
-                    <h3 class="section-title">Welcome Dear Visitor in <?= SITE_NAME ?></h3>
-                    <p class="section-description">
-                        We are dedicated to give optimized and cost effective product within a scheduled time, We have a very skilled, enthusiastic and experienced team to develop high quality product.
+<section class="features-section fancy-shadow" style="background:none">
+                <div class="testimonials-sections" style="padding-left: 141px;
+    padding-right: 67px;">
+                    <div class="col-md-7">
+                         <h3 class="section-title" style="text-align: left;">Welcome Dear Visitor in <?= SITE_NAME ?></h3>
+                         <p class="section-description" style="text-align: justify;">
+                        We are dedicated to give optimized and cost effective product within a scheduled time, We have a very skilled, enthusiastic and experienced team to develop high quality product. <br><br>TechNets is a value-driven IT company specialized in web designing and development, search engine optimizations & web promotion. We deliver years of domain expertise and unmatched value to our clients. Our solutions power operations of clients across critical industries such as ecommerce, matrimony, education, hospitality and courier. We are one stop solution for your IT needs in critical Mobile app, responsive Web application , Cloud/ SaaS development and domain-hosting
                     </p><!-- End Section Description -->
+                    </div>
+                    <div class="col-md-5 row">
+                        <img src="<?= SRC_URL ?>img/backg.jpg" class="img-responsive"/>
+<!--                        <div class="col-md-6" style="padding:10px;"><img src="<?= SRC_URL ?>img/dd.jpg" class="img-responsive"></div>
+                         <div class="col-md-6" style="padding:10px"><img src="<?= SRC_URL ?>img/backgorund-linear.png" class="img-responsive"></div>
+                          <div class="col-md-6" style="padding:10px"><img src="<?= SRC_URL ?>img/backgorund-linear.png" class="img-responsive"></div>
+                           <div class="col-md-6" style="padding:10px"><img src="<?= SRC_URL ?>img/backgorund-linear.png" class="img-responsive"></div>-->
+                    </div>                    
+
                 </div>
-                <div class="section-content features-content fadeInDown-animation">
-                    <div class="container">
-                        <div class="row">
+    <div class="section-content features-content fadeInDown-animation" >
+                    <div class="container" style="border:0px">
+                        <div class="row" style="margin-bottom: 60px;">
+                            <div class="col-md-12" style="margin-top:70px;">&nbsp;</div>
+                            <h1 style="font-size: 40px;">Custome Software Development Company</h1>
+                            <p style="font-size: 18px;margin-bottom:30px;">We Offer Web Development & Mobile Application Services</p>
+                            
                             <div class="col-md-3 col-xs-6">
                                 <div class="feature-box alt">
-                                    <div class="icon"><img src="<?= SRC_URL ?>img/icons/feature-icon-1.png" class="es-tr" alt=""></div><!-- End Icon -->
+                                    <div class="icon"><img src="<?= SRC_URL ?>img/webdevelopment.png" class="es-tr" alt=""></div><!-- End Icon -->
                                     <h5 class="feature-title">Web Development</h5>
                                     <p class="feature-description">
                                         We are mainly focused on Web Development, websites( static or dynamic), web applications and web services. Courier websites and parcel tracking systems etc.
@@ -21,7 +36,7 @@
                             </div>
                             <div class="col-md-3 col-xs-6">
                                 <div class="feature-box alt">
-                                    <div class="icon"><img src="<?= SRC_URL ?>img/icons/feature-icon-2.png" class="es-tr" alt=""></div><!-- End Icon -->
+                                    <div class="icon"><img src="<?= SRC_URL ?>img/logodesigning.png" class="es-tr" alt=""></div><!-- End Icon -->
                                     <h5 class="feature-title">Web & Logo Designing</h5>
                                     <p class="feature-description">
                                       Web & Logo designing, mobile responsive designing, using native css and css framworks like Bootstrap, Foundation, Tailwind CSS etc.
@@ -30,7 +45,7 @@
                             </div>
                             <div class="col-md-3 col-xs-6">
                                 <div class="feature-box alt">
-                                    <div class="icon"><img src="<?= SRC_URL ?>img/icons/feature-icon-3.png" class="es-tr" alt=""></div><!-- End Icon -->
+                                    <div class="icon"><img src="<?= SRC_URL ?>img/cmscosti.png" class="es-tr" alt=""></div><!-- End Icon -->
                                     <h5 class="feature-title">CMS Customization</h5>
                                     <p class="feature-description">
                                         We also works on various CMS like Wordpress, Magento, Drupal etc.
@@ -39,7 +54,7 @@
                             </div>
                             <div class="col-md-3 col-xs-6">
                                 <div class="feature-box alt">
-                                    <div class="icon"><img src="<?= SRC_URL ?>img/icons/feature-icon-4.png" class="es-tr" alt=""></div><!-- End Icon -->
+                                    <div class="icon"><img src="<?= SRC_URL ?>img/webhosting.png" class="es-tr" alt=""></div><!-- End Icon -->
                                     <h5 class="feature-title">Web Hosting</h5>
                                     <p class="feature-description">
                                         We provide web hosting on, both linux and windows platform, in very affordable price.
@@ -53,59 +68,139 @@
 
             <div class="clearfix"></div>
 
-            <section class="full-section instructors-section fancy-shadow">
+            <section class="instructors-section fancy-shadow" style="border:0px;">
                 <div class="container">
                     <h3 class="section-title">Web Hosting with <?= SITE_NAME ?></h3>
                     <p class="section-description">
                        We provide web hosting on winndow and linux plateforms with various price segment. Below is our hosting plans
                     </p><!-- End Section Description -->
                 </div>
-                <div class="section-content instructors-content fadeInDown-animation">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-3 col-xs-6">
-                                <div class="instructor">
-                                    <div class="avatar"></div><!-- End Avatar -->
-                                    <div class="instructor-info">
-                                        <p class="name"><img src="<?= SRC_URL ?>img/windows.jpg" class="img-responsive service-img" alt="">Windows (1 year)</p>
-                                        <span class="position">INR 4000 / year</span>
+                <section class="pricing-tables" style="background: none;padding:20px;">
+                <div class="container">
+                    <div class="row table-row fadeInDown-animation table-1">
+                        <div class="col-md-3 col-sm-6">
+                            <div class="table">
 
-                                    </div>
-                                </div><!-- End Instructor Box -->
-                            </div>
-                            <div class="col-md-3 col-xs-6">
-                                <div class="instructor">
-                                    <div class="avatar"></div><!-- End Avatar -->
-                                    <div class="instructor-info">
-                                        <p class="name"><img src="<?= SRC_URL ?>img/windows.jpg" class="img-responsive service-img" alt="">Windows (3 year)</p>
-                                        <span class="position">INR 3,500 / years</span>
+                                <div class="table-header grad-btn">
+                                    <div class="icon ln-tr"><i class="fa fa-cloud"></i></div><!-- end icon -->
+                                    <p class="text">Standard Plan</p><!-- end text -->
+                                </div><!-- end table header -->
 
-                                    </div>
-                                </div><!-- End Instructor Box -->
-                            </div>
-                            <div class="col-md-3 col-xs-6">
-                                <div class="instructor">
-                                    <div class="avatar"></div><!-- End Avatar -->
-                                    <div class="instructor-info">
-                                        <p class="name"><img src="<?= SRC_URL ?>img/linux.png" class="img-responsive service-img" alt="">Linux (1 year)</p>
-                                        <span class="position">INR 3500 / year</span>
+                                <div class="table-body">
+                                    <ul class="features">
+                                        <li>&nbsp;</li>
+                                        <li>200 Mb Space</li>
+                                        <li>2 email accounts</li>
+                                        <li>Single FTP accounts</li>
+                                        <li>Enhanced security</li>
+                                        <li>24/7 Suport</li>
+                                    </ul><!-- end features list -->
+                                </div><!-- end table body -->
 
-                                    </div>
-                                </div><!-- End Instructor Box -->
-                            </div>
-                            <div class="col-md-3 col-xs-6">
-                                <div class="instructor">
-                                    <div class="avatar"></div><!-- End Avatar -->
-                                    <div class="instructor-info">
-                                        <p class="name"><img src="<?= SRC_URL ?>img/linux.png" class="img-responsive service-img" alt="">Linux (3 year)</p>
-                                        <span class="position">INR 3,000 / year</span>
+                                <div class="table-footer">
+                                    <div class="order-btn">
+                                        <a href="#" class="grad-btn ln-tr">999 <span class="currency">Rs / Year</span>
+                                            <span class="icon fr ln-tr"><i class="fa fa-angle-right"></i></span>
+                                        </a>
+                                    </div><!-- end order button -->
+                                </div><!-- end table footer -->
 
-                                    </div>
-                                </div><!-- End Instructor Box -->
-                            </div>
-                        </div>
-                    </div>
-                </div><!-- End Instructors Section Content -->
+                            </div><!-- end table -->
+                        </div><!-- end col-md-3 col-sm-6 -->
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="table">
+
+                                <div class="table-header grad-btn">
+                                    <div class="icon ln-tr"><i class="fa fa-cloud"></i></div><!-- end icon -->
+                                    <p class="text">Basic Plan</p><!-- end text -->
+                                </div><!-- end table header -->
+
+                                <div class="table-body">
+                                    <ul class="features">
+                                        <li>&nbsp;</li>
+                                        <li>500 Mb Space</li>
+                                        <li>5 email accounts</li>
+                                        <li>2 FTP accounts</li>
+                                        <li>Enhanced security</li>
+                                        <li>24/7 Suport</li>
+                                    </ul><!-- end features list -->
+                                </div><!-- end table body -->
+
+                                <div class="table-footer">
+                                    <div class="order-btn">
+                                        <a href="#" class="grad-btn ln-tr">1500 <span class="currency">Rs / Year</span>
+                                            <span class="icon fr ln-tr"><i class="fa fa-angle-right"></i></span>
+                                        </a>
+                                    </div><!-- end order button -->
+                                </div><!-- end table footer -->
+
+                            </div><!-- end table -->
+                        </div><!-- end col-md-3 col-sm-6 -->
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="table">
+
+                                <div class="table-header grad-btn">
+                                    <div class="icon ln-tr"><i class="fa fa-cloud"></i></div><!-- end icon -->
+                                    <p class="text">Professional Plan</p><!-- end text -->
+                                </div><!-- end table header -->
+
+                                <div class="table-body">
+                                    <ul class="features">
+                                        <li>Hosting acounts</li>
+                                        <li>1 GB Space</li>
+                                        <li>10 email accounts</li>
+                                        <li>2 FTP accounts</li>
+                                        <li>Enhanced security</li>
+                                        <li>24/7 Suport</li>
+                                    </ul><!-- end features list -->
+                                </div><!-- end table body -->
+
+                                <div class="table-footer">
+                                    <div class="order-btn">
+                                        <a href="#" class="grad-btn ln-tr">2999 <span class="currency">Rs / Year</span>
+                                            <span class="icon fr ln-tr"><i class="fa fa-angle-right"></i></span>
+                                        </a>
+                                    </div><!-- end order button -->
+                                </div><!-- end table footer -->
+
+                            </div><!-- end table -->
+                        </div><!-- end col-md-3 col-sm-6 -->
+
+                        <div class="col-md-3 col-sm-6">
+                            <div class="table">
+
+                                <div class="table-header grad-btn">
+                                    <div class="icon ln-tr"><i class="fa fa-cloud"></i></div><!-- end icon -->
+                                    <p class="text">Ultra Plan</p><!-- end text -->
+                                </div><!-- end table header -->
+
+                                <div class="table-body">
+                                    <ul class="features">
+                                        <li>Hosting acounts</li>
+                                        <li>5 GB Space</li>
+                                        <li>Unlimited email accounts</li>
+                                        <li>Unlimited FTP accounts</li>
+                                        <li>Enhanced security</li>
+                                        <li>24/7 Suport</li>
+                                    </ul><!-- end features list -->
+                                </div><!-- end table body -->
+
+                                <div class="table-footer">
+                                    <div class="order-btn">
+                                        <a href="#" class="grad-btn ln-tr">5000 <span class="currency">Rs / Year</span>
+                                            <span class="icon fr ln-tr"><i class="fa fa-angle-right"></i></span>
+                                        </a>
+                                    </div><!-- end order button -->
+                                </div><!-- end table footer -->
+
+                            </div><!-- end table -->
+                        </div><!-- end col-md-3 col-sm-6 -->
+                    </div><!-- end 1st row -->
+                  
+                </div><!-- end container -->
+            </section><!-- End Instructors Section Content -->
             </section><!-- End Our Instructors Container -->
 
             <div class="clearfix"></div>
