4e11ebd9655d3407fc8ad3f8737debb0bb014d66
Sujeet Kumar Kahar <kahar26sujeet@gmail.com>
2020-08-24 01:37:04 +0530

    service update

---
+++ view/user/mobile_application.php
@@ -0,0 +1,231 @@
+
+<style>
+    
+</style>
+            <div class="inner-head">
+                <div class="container">
+                    <h1 class="entry-title">Mobile Application</h1>
+                    <p class="description">
+                       We can start explaining mobile development, which is not about building phone apps, though it is a huge part of it.
+Actually, It's doing any reasonably development for any kind of mobile devices such as developing apps for phones, tablets and every form of wearable devices that run any kind of mobile operating system.
+                    </p>
+                    <div class="breadcrumb">
+                        <ul class="clearfix">
+                            <li class="ib"><a href="">Home</a></li>
+                         
+                            <li class="ib current-page"><a href="">Mobile Application</a></li>
+                        </ul>
+                    </div>
+                </div><!-- End container -->
+            </div><!-- End Inner Page Head -->
+
+            <div class="clearfix"></div>
+
+            <section class="full-section about-section fancy-shadow">
+                <div class="about-content fadeInDown-animation">
+                    <div class="container">
+                        <div class="row">
+                             <div class="col-md-5" style="text-align:right">
+                                <div class="about-images">
+                                    <div class="main-img"><img class="img-responsive" src="<?= SRC_URL ?>img/services/mob_development.png" alt="" style="height: 282px;"></div><!-- Main Image -->
+                                </div>
+                            </div>
+                           
+                            <div class="col-md-7">
+                               
+                                <div class="description">
+                                    <p style="text-align:justify">
+                                       We can start explaining mobile development, which is not about building phone apps, though it is a huge part of it.
+Actually, It's doing any reasonably development for any kind of mobile devices such as developing apps for phones, tablets, 
+smartwatches, and every form of wearable devices that run any kind of mobile operating system.
+<br>
+<br>
+Mobile development presents a reasonably distinctive chance for a one-person development team to build an actual, usable, 
+significant app end-to-end during a comparatively short period. However, mobile apps development represents more than just a 
+chance for the solo-developer to create their own project as it is arguably the longer term of development, as mobile devices are 
+getting larger and bigger parts of our lives.
+<br>
+<br>
+In this post, we will take a glance at what mobile app development is, 
+check a number of the major mobile platforms, and speak a bit regarding the technologies that exist for developing so 
+you will have a clear idea of whether is it a good match for you or not.</p>
+                                   
+                                    
+                                </div>
+                            </div>
+                           
+                        </div>
+                    </div>
+                </div><!-- End Text & Image Section Content -->
+            </section>
+            <section class="instructors-section"style="border:0px;">
+                <div class="container">
+                     <h1 class="h1-title" style="color:#353a48;margin-bottom:30px;text-align: left;">Major Mobile Development Platforms</h1>
+                    <p class="section-description">
+                    </p><!-- End Section Description -->
+                </div>
+                <div class="section-content instructors-content fadeInDown-animation">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 col-xs-12 col-sm-12" style="border: 1px solid #cece5b;padding-top: 10px;">
+                               <div class="col-md-2 col-xs-12 col-sm-12">
+                                    <div class="">
+                                        <img src="<?= SRC_URL ?>img/services/android.png" class="img-responsive"  alt="" style="height:127px;">
+                                        <p class="name" style="font-size: 15px;font-weight: 700;margin-right: 29px;">Android</p>
+                                    </div><!-- End Avatar -->
+                               </div>
+                                <div class="col-md-10 col-xs-12 col-sm-12">
+                                    <div class="instructor-info" style="background: none;padding: 27px 20px 30px;">
+                                       
+                                        <span class="position" style="text-align: justify;line-height: 20px;padding:0px 10px 0px 10px">Android is the other dominant player in this space, it was a bit later participant to the game, first being released in Sept 2008, virtually a year later than iOS but it has managed to achieve a reasonably massive share of the mobile market.
+
+Technically, Android is the mobile OS with the largest most dominant share of the market with around  80% share compared to iOS's 18 % share. Those numbers are a bit deceiving since android may be a fragmented market consisting of the many different devices created by different manufacturers, running completely different versions of the Android OS.
+</span>
+                                        
+                                    </div>
+                               </div>
+                                
+                            </div>
+                            <div class="col-md-12 col-xs-12 col-sm-12" style="border: 1px solid #cece5b;margin-top: 10px;padding-top: 10px;">
+                               <div class="col-md-2 col-xs-12 col-sm-12">
+                                    <div class="">
+                                        <img src="<?= SRC_URL ?>img/services/Apple.png" class="img-responsive"  alt="" style="height:122px;">
+                                        <p class="name" style="font-size: 15px;font-weight: 700;margin-right: 29px;">iOS</p>
+                                    </div><!-- End Avatar -->
+                               </div>
+                                <div class="col-md-10 col-xs-12 col-sm-12">
+                                    <div class="instructor-info" style="background: none;padding: 27px 20px 30px;">
+                                       
+                                        <span class="position" style="text-align: justify;line-height: 20px;padding:0px 10px 0px 10px">iOS is kind of arguably the "big dog" once it comes to major app development platforms, partially because it had been the platform that finally brought mobile development into the modern day and age by fully reworking the concept of a mobile device and mobile software system. iOS is of course developed by Apple, and it runs exclusively on Apple products.
+
+Apple provides iOS developers with many native tools and libraries to develop iOS applications, and, though you do not have to be enforced to use Apple's development tools to create your apps, you just need to have a mac running OS X to build your application.
+</span>
+                                        
+                                    </div>
+                               </div>
+                                
+                            </div>
+                           
+                           
+                           
+                        </div>
+                    </div>
+                </div><!-- End Instructors Section Content -->
+            </section>
+
+              <section class="instructors-section"style="border:0px;">
+                <div class="container">
+                     <h1 class="h1-title" style="color:#353a48;text-align: left;margin-top: 20px;">What are the primary differences between iOS and Android?</h1>
+                     <div class="col-md-12 col-xs-12 col-sm-12" style="">
+                               
+                                <div class="col-md-10 col-xs-12 col-sm-12">
+                                    <div class="instructor-info" style="background: none;padding: 27px 20px 30px;">
+                                       
+                                        <span class="position" style="text-align: justify;line-height: 20px;padding:0px 10px 0px 10px">
+                                            <ol style="list-style-type: square;">
+                                                <li>Android is backed by Google.</li>
+                                                <li>iOS is backed by Apple.</li>
+                                                <li>Anyone can build an Android device, and it is designed to run on a variety of different hardware platforms and devices with very different form factors and capabilities.</li>
+                                               <li>iOS is designed to run only on a specific set of Apple devices.</li>
+                                                 <li>Android is based on the Linux kernel, and Google releases the source code for Android as open source.</li>
+                                                   <li>Like Apple, Google provides some native tools for Android development but again you are not required to use them.</li>
+                                            
+                                            </ol>
+                                        </span>
+                                        
+                                    </div>
+                               </div>
+                                <div class="col-md-2 col-xs-12 col-sm-12">
+                                    <div class="">
+                                        <img src="<?= SRC_URL ?>img/services/android_ios.png" class="img-responsive"  alt="" style="height:122px;">
+                                        
+                                    </div><!-- End Avatar -->
+                               </div>
+                            </div>
+                </div>
+                 
+                           
+            </section>
+<!--            <section class="about-services-section">
+<div class="container">
+<h2>Our Services</h2>
+<ul class="about-service-listing">
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/custom-software-development" title="Custom Software Development"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/enterprise_solution.svg" alt="about-service-icons">
+</em>
+<p><span>Custom Software</span> Development</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/web-development" title="Web Development"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/application_development.svg" alt="about-service-icons">
+</em>
+<p>Web Development</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/dedicated-development-team" title="Dedicated Development Team"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/development_center.svg" alt="about-service-icons">
+</em>
+<p><span>Dedicated Development</span> Team</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/product-development-maintenance" title="Product Development"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/cloud_service.svg" alt="about-service-icons">
+</em>
+<p>Product Development</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/ecommerce-development" title="E-Commerce Development"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/e-commerce.svg" alt="about-service-icons">
+</em>
+<p>E-Commerce</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/mobile-app-development" title="Mobile Apps"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/mobile_app.svg" alt="about-service-icons">
+</em>
+<p>Mobile Apps</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/testing-qa" title="Testing &amp; QA"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/testing-qa.svg" alt="about-service-icons">
+</em>
+<p>Testing &amp; QA</p>
+</div>
+</li>
+<li>
+<div class="about-service">
+<a href="https://www.tatvasoft.com/software-development-services/ui-ux-design-services" title="UI/UX Design"></a>
+<em>
+<img src="https://www.tatvasoft.com/public/images/ui_ux_design.svg" alt="about-service-icons">
+</em>
+<p>UI/UX Design</p>
+</div>
+</li>
+ </ul>
+</div>
+</section>-->
+            
+            
+            <div class="clearfix"></div>
+
